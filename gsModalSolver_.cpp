c85e073205e591196232cb5fb1a7952c883ab57f
Hugo Verhelst <hugoverhelst1995@gmail.com>
2021-03-16 14:27:41 +0100

    moved initializations

---
+++ gsModalSolver_.cpp
@@ -0,0 +1,11 @@
+#include <gismo.h>
+
+#include <gsCore/gsTemplateTools.h>
+
+#include <gsStructuralAnalysis/gsModalSolver.h>
+#include <gsStructuralAnalysis/gsModalSolver.hpp>
+
+namespace gismo
+{
+		CLASS_TEMPLATE_INST gsModalSolver<real_t>;
+}
