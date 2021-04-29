64df0f867dd4f8f3385fde5487fae45242e18e1c
Hugo Verhelst <hugoverhelst1995@gmail.com>
2021-04-29 10:46:29 +0200

    Fix spectra solvers for shiftInvert, Cayley and Buckling

--- gsBucklingSolver.hpp
+++ gsBucklingSolver.hpp
@@ -24,11 +24,11 @@ template <class T, Spectra::GEigsMode GEigsMode>
 void gsBucklingSolver<T,GEigsMode>::initializeMatrix()
 {
   if (m_verbose) { gsInfo<<"Computing matrices" ; }
-  m_solver.compute(m_linear);
+  m_solver.compute(m_A);
   if (m_verbose) { gsInfo<<"." ; }
   m_solVec = m_solver.solve(m_scaling*m_rhs);
   if (m_verbose) { gsInfo<<"." ; }
-  m_nonlinear = m_nonlinearFun(m_solVec);
+  m_B = m_nonlinearFun(m_solVec)-m_A;
   if (m_verbose) { gsInfo<<"." ; }
   if (m_verbose) { gsInfo<<"Finished\n" ; }
 };
@@ -37,7 +37,7 @@ template <class T, Spectra::GEigsMode GEigsMode>
 void gsBucklingSolver<T,GEigsMode>::compute(T shift)
 {
     if (m_verbose) { gsInfo<<"Solving eigenvalue problem" ; }
-    m_eigSolver.compute(m_linear-shift*(m_nonlinear - m_linear),m_nonlinear - m_linear);
+    m_eigSolver.compute(m_A-shift*m_B,m_B);
     if (m_verbose) { gsInfo<<"." ; }
     m_values  = m_eigSolver.eigenvalues();
     m_values.array() += shift;
@@ -53,16 +53,14 @@ typename std::enable_if<_GEigsMode==Spectra::GEigsMode::Cholesky ||
                         _GEigsMode==Spectra::GEigsMode::RegularInverse
                         ,
                         void>::type
-gsBucklingSolver<T,GEigsMode>::computeSparse_impl(T shift, index_t number)
+gsBucklingSolver<T,GEigsMode>::computeSparse_impl(T shift, index_t number, index_t ncvFac, Spectra::SortRule selectionRule, Spectra::SortRule sortRule)
 {
   if (m_verbose) { gsInfo<<"Solving eigenvalue problem" ; }
-  gsSparseMatrix<T> lhs = m_linear-shift*(m_nonlinear - m_linear);
-  gsSpectraGenSymSolver<gsSparseMatrix<T>,GEigsMode> solver(lhs,m_nonlinear - m_linear,number,2*number);
-  // gsSpectraGenSymShiftSolver<gsSparseMatrix<T>,Spectra::GEigsMode::ShiftInvert> solver(m_linear,m_nonlinear - m_linear,number,2*number,shift);
+  gsSpectraGenSymSolver<gsSparseMatrix<T>,GEigsMode> solver(m_A-shift*m_B,m_B,number,2*number);
   if (m_verbose) { gsInfo<<"." ; }
   solver.init();
   if (m_verbose) { gsInfo<<"." ; }
-  solver.compute(Spectra::SortRule::SmallestMagn,1000,1e-6,Spectra::SortRule::SmallestMagn);
+  solver.compute(selectionRule,1000,1e-6,sortRule);
 
   if (solver.info()==Spectra::CompInfo::Successful)
     gsDebug<<"Spectra converged in "<<solver.num_iterations()<<" iterations and with "<<solver.num_operations()<<"operations. \n";
@@ -90,18 +88,22 @@ typename std::enable_if<_GEigsMode==Spectra::GEigsMode::ShiftInvert ||
                         _GEigsMode==Spectra::GEigsMode::Cayley
                         ,
                         void>::type
-gsBucklingSolver<T,GEigsMode>::computeSparse_impl(T shift, index_t number)
+gsBucklingSolver<T,GEigsMode>::computeSparse_impl(T shift, index_t number, index_t ncvFac, Spectra::SortRule selectionRule, Spectra::SortRule sortRule)
 {
+  if (selectionRule == Spectra::SortRule::SmallestMagn )
+    gsWarn<<"Selection Rule 'SmallestMagn' is selected, but for ShiftInvert, Buckling and Cayley it is advised to use 'LargestMagn'!\n";
+  if (selectionRule == Spectra::SortRule::SmallestAlge )
+    gsWarn<<"Selection Rule 'SmallestAlge' is selected, but for ShiftInvert, Buckling and Cayley it is advised to use 'LargestMagn'!\n";
+
   if (m_verbose) { gsInfo<<"Solving eigenvalue problem" ; }
-  gsSpectraGenSymShiftSolver<gsSparseMatrix<T>,GEigsMode> solver(m_linear,m_nonlinear - m_linear,number,2*number,shift);
-  // gsSpectraGenSymShiftSolver<gsSparseMatrix<T>,Spectra::GEigsMode::ShiftInvert> solver(m_linear,m_nonlinear - m_linear,number,2*number,shift);
+  gsSpectraGenSymShiftSolver<gsSparseMatrix<T>,GEigsMode> solver(m_A,m_B,math::floor(m_A.cols()/2),m_A.cols(),shift);
   if (m_verbose) { gsInfo<<"." ; }
   solver.init();
   if (m_verbose) { gsInfo<<"." ; }
-  solver.compute(Spectra::SortRule::SmallestMagn,1000,1e-6,Spectra::SortRule::SmallestMagn);
+  solver.compute(selectionRule,1000,1e-6,sortRule);
 
   if (solver.info()==Spectra::CompInfo::Successful)
-    gsDebug<<"Spectra converged in "<<solver.num_iterations()<<" iterations and with "<<solver.num_operations()<<"operations. \n";
+    gsDebug<<"\nSpectra converged in "<<solver.num_iterations()<<" iterations and with "<<solver.num_operations()<<"operations. \n";
   else if (solver.info()==Spectra::CompInfo::NumericalIssue)
     GISMO_ERROR("Spectra did not converge! Error code: NumericalIssue");
   else if (solver.info()==Spectra::CompInfo::NotConverging)
@@ -122,7 +124,7 @@ template <class T, Spectra::GEigsMode GEigsMode>
 void gsBucklingSolver<T,GEigsMode>::computePower()
 {
     if (m_verbose) { gsInfo<<"Solving eigenvalue problem" ; }
-    gsMatrix<T> D = m_linear.toDense().inverse() * (m_nonlinear.toDense() - m_linear.toDense());
+    gsMatrix<T> D = m_A.toDense().inverse() * (m_B);
 
     gsVector<T> v(D.cols());
     v.setOnes();
