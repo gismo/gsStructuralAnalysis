04648993eb640370ab6a57945d0bacaddb9d0556
Hugo Verhelst <hugoverhelst1995@gmail.com>
2021-06-07 08:36:27 +0200

    [WIP] added base classes 2

--- gsArcLengthIterator_.cpp
+++ gsArcLengthIterator_.cpp
@@ -2,10 +2,35 @@
 
 #include <gsCore/gsTemplateTools.h>
 
+#include <gsStructuralAnalysis/gsALMBase.h>
+#include <gsStructuralAnalysis/gsALMBase.hpp>
+
 #include <gsStructuralAnalysis/gsArcLengthIterator.h>
 #include <gsStructuralAnalysis/gsArcLengthIterator.hpp>
+
+#include <gsStructuralAnalysis/gsALMLoadControl.h>
+#include <gsStructuralAnalysis/gsALMLoadControl.hpp>
+
+#include <gsStructuralAnalysis/gsALMRiks.h>
+#include <gsStructuralAnalysis/gsALMRiks.hpp>
+
+#include <gsStructuralAnalysis/gsALMCrisfield.h>
+#include <gsStructuralAnalysis/gsALMCrisfield.hpp>
+
+#include <gsStructuralAnalysis/gsALMConsistentCrisfield.h>
+#include <gsStructuralAnalysis/gsALMConsistentCrisfield.hpp>
+
+#include <gsStructuralAnalysis/gsALMExplicitIterations.h>
+#include <gsStructuralAnalysis/gsALMExplicitIterations.hpp>
 
 namespace gismo
 {
 		CLASS_TEMPLATE_INST gsArcLengthIterator<real_t>;
+
+		CLASS_TEMPLATE_INST gsALMBase<real_t>;
+		CLASS_TEMPLATE_INST gsALMLoadControl<real_t>;
+		CLASS_TEMPLATE_INST gsALMRiks<real_t>;
+		CLASS_TEMPLATE_INST gsALMCrisfield<real_t>;
+		CLASS_TEMPLATE_INST gsALMConsistentCrisfield<real_t>;
+		CLASS_TEMPLATE_INST gsALMExplicitIterations<real_t>;
 }
