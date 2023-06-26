9f892355c5cd488b4c54e0dd69114e76a8c7599f
Hugo Verhelst <hugoverhelst1995@gmail.com>
2023-06-26 10:45:23 +0200

    * Improvements in static solver: allow for composite (N-stage) solves * Small changes in snapping DC code

--- gsStaticBase_.cpp
+++ gsStaticBase_.cpp
@@ -9,9 +9,13 @@
 #include <gsStructuralAnalysis/gsStaticNewton.h>
 #include <gsStructuralAnalysis/gsStaticNewton.hpp>
 
+#include <gsStructuralAnalysis/gsStaticComposite.h>
+#include <gsStructuralAnalysis/gsStaticComposite.hpp>
+
 namespace gismo
 {
 		CLASS_TEMPLATE_INST gsStaticBase<real_t>;
 		CLASS_TEMPLATE_INST gsStaticDR<real_t>;
 		CLASS_TEMPLATE_INST gsStaticNewton<real_t>;
+		CLASS_TEMPLATE_INST gsStaticComposite<real_t>;
 }
