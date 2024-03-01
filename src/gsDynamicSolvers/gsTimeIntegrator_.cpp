fa01c2f648fc1cf3b651bff8863e4b1686da0045
Hugo Verhelst <hugoverhelst1995@gmail.com>
2024-03-01 14:35:16 +0100

    remove gsTimeIntegrator

--- src/gsDynamicSolvers/gsTimeIntegrator_.cpp
+++
@@ -1,9 +0,0 @@
-#include <gsCore/gsTemplateTools.h>
-
-#include <gsStructuralAnalysis/src/gsDynamicSolvers/gsTimeIntegrator.h>
-#include <gsStructuralAnalysis/src/gsDynamicSolvers/gsTimeIntegrator.hpp>
-
-namespace gismo
-{
-		CLASS_TEMPLATE_INST gsTimeIntegrator<real_t>;
-}
